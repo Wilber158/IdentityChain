3aef6f9b34b4bb836d773ae35419b884ea9d333c
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-04-12 20:37:29 -0400

    GUI changes

---
+++ readme.txt
@@ -0,0 +1,2 @@
+pip install pycryptodome
+pip install eel
---
+++ transactions.py
@@ -0,0 +1,10 @@
+import keys
+
+
+class Transactions:
+    def __init__(self, sender, receiver, transaction):
+        self.sender = sender
+        self.receiver = receiver
+        self.transaction = transaction
+
+    
