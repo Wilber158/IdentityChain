dd1d0a5ecff3df5166d7b11f1156187f6ab54c06
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-04-17 17:03:23 -0400

    p2p network

--- P2P/protocol.py
+++ P2P/protocol.py
@@ -1,7 +1,27 @@
-from p2pnetwork import node
+from p2pnetwork.node import Node
 
+class MyOwnPeer2PeerNode (Node):
+    # Python class constructor
+    def __init__(self, host, port, id=None, callback=None, max_connections=0):
+        super(MyOwnPeer2PeerNode, self).__init__(host, port, id, callback, max_connections)
 
-class MyNode(node):
+    def outbound_node_connected(self, connected_node):
+        print("outbound_node_connected: " + connected_node.id)
+        
+    def inbound_node_connected(self, connected_node):
+        print("inbound_node_connected: " + connected_node.id)
 
-    def __init__(self, host, ):
-        super().__init__()
+    def inbound_node_disconnected(self, connected_node):
+        print("inbound_node_disconnected: " + connected_node.id)
+
+    def outbound_node_disconnected(self, connected_node):
+        print("outbound_node_disconnected: " + connected_node.id)
+
+    def node_message(self, connected_node, data):
+        print("node_message from " + connected_node.id + ": " + str(data))
+        
+    def node_disconnect_with_outbound_node(self, connected_node):
+        print("node wants to disconnect with oher outbound node: " + connected_node.id)
+        
+    def node_request_to_stop(self):
+        print("node is requested to stop!")
