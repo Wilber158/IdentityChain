f9380713b13007249fe5bcc00397567c436361e2
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-04-20 14:52:26 -0400

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,6 +1,7 @@
 import keys
 
 class Transactions:
+<<<<<<< Updated upstream
     def __init__(self, sender, receiver, transaction):
         self.sender_public_key = sender
         self.receiver_public_key = receiver
@@ -8,5 +9,12 @@ class Transactions:
 
     def encrypt(transaction):
         return keys.generate_signiture()
+=======
+    def __init__(self, sender, receiver, type, transaction):
+        self.sender = sender
+        self.receiver = receiver
+        self.type = type
+        self.transaction = transaction
+>>>>>>> Stashed changes
 
     
