c977374f593f41891e54520965eed28c8075cd25
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-04-27 10:05:07 -0400

    added redirect to html pages

--- web/main.js
+++ web/main.js
@@ -5,12 +5,15 @@ document.getElementById("register-btn").addEventListener("click", function() {
 
 document.getElementById("share-btn").addEventListener("click", function() {
     // Add your share button functionality here
+    window.location.href = "share.html"
 });
 
 document.getElementById("update-btn").addEventListener("click", function() {
     // Add your update button functionality here
+    window.location.href = "updatePage.html"
 });
 
 document.getElementById("manage-btn").addEventListener("click", function() {
     // Add your manage button functionality here
+    
 });
